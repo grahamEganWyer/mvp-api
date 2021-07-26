27a7c9025b7dcce422578acb2b361ad2a4d24434
Graham <graham@Daniel.local>
2021-07-26 17:34:05 +1000

    2nd api added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const Constants = require('./items');
 
 const port = process.env.PORT || 3001;
 const app = express();
@@ -16,17 +17,13 @@ app.get('/api/warcraftlogs-client', (req, res) => {
         password: process.env.CLIENT_SECRET,
       },
     })
-    .then((response) => {
-      console.log('first promisde');
-      console.log(req);
-      return axios.post('https://classic.warcraftlogs.com/api/v2/client', {
-        query: `query { characterData { character( name: "${req.query.name}", serverSlug: "${req.query.serverSlug}", serverRegion: "${req.query.serverRegion}") {zoneRankings(byBracket: true)}} }`,
-      }, {
-        headers: {
-          Authorization: `Bearer ${response.data.access_token}`,
-        },
-      });
-    })
+    .then((response) => axios.post('https://classic.warcraftlogs.com/api/v2/client', {
+      query: `query { characterData { character( name: "${req.query.name}", serverSlug: "${req.query.serverSlug}", serverRegion: "${req.query.serverRegion}") {zoneRankings(byBracket: true)}} }`,
+    }, {
+      headers: {
+        Authorization: `Bearer ${response.data.access_token}`,
+      },
+    }))
     // eslint-disable-next-line consistent-return
     .then((response) => {
       if (response.data.data.characterData.character) {
@@ -37,6 +34,38 @@ app.get('/api/warcraftlogs-client', (req, res) => {
     .then((data) => {
       res.json(data);
     });
+});
+
+app.get('/api/nexus-hub', async (req, res) => {
+  console.log(req.body);
+  console.log('helo itmes');
+  // const response = await axios
+  //   .get('https://api.nexushub.co/wow-classic/v1/items/yojamba-horde', {
+  //     headers: {
+  //       Authorization: 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY3AiOiIiLCJ1aWQiOiJiZWFzdGhlYXJ0IiwiaWF0IjoxNjI2OTYwMDk0LCJleHAiOjE2MjY5NjM2OTR9.q6oJQLIjpaDCwE7dej_XUnLeaQZSKSsCQv5JBJ-1VMIdeivwcqOLXd2bK9b1Bm_K6e5_Q9Iw45bUw9a5cxE22SniIKE4M5rlL03rB4oOC_HheFV2zeQYqxHgy6DQaJ27aHoQQa1DFxIdMvixIlzH9xnzsVYeRDPmG4O6o0OFmGYltmyjsYNP5J-D_rqU8SsHn1uCCaKvaeC3-OY7lNKtPot2CZl4x9P7zLPuDiff0rxaqoWXqk-_k2nZt3yQzZEv2rvQDWR3Gf0OB9XhYBRVpCPkmW1hMmm7n_OJhfdUiKNPmR2raZg20otC4U6dwfv3WN5U82oNjfGLSNbczOUmdA',
+  //     },
+  //   });
+  // console.log(JSON.stringify(response.data.data.map((item) => item.itemId)));
+  const allItemIds = Constants.ITEMS;
+  const lowerBound = Math.floor((Math.random() * (allItemIds.length - 10)));
+  const itemIds = allItemIds.slice(lowerBound, lowerBound + 10);
+  const requests = itemIds.map((id) => axios.get(`https://api.nexushub.co/wow-classic/v1/items/yojamba-horde/${id}`));
+  console.log(requests);
+
+  const items = await Promise.all(requests)
+    .then((itemArray) => itemArray.map((i) => ({
+      name: i.data.name,
+      icon: i.data.icon,
+      sellPrice: i.data.sellPrice,
+      vendorPrice: i.data.vendorPrice,
+      tooltip: i.data.tooltip[0].label,
+      itemLevel: i.data.itemLevel,
+      tags: i.data.tags[0],
+      stats: i.data.stats,
+    })));
+
+  console.log(items);
+  res.json(items);
 });
 
 if (process.env.NODE_ENV === 'production') {
